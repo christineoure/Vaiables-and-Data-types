4f7bd0968b3951b2d350611c0d82a649cf396b1d
christineoure <ourechristine@gmail.com>
2022-02-12 09:12:33 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+    var name = "Santana Miranda"
+    println (name)
+    var age = "21"
+    println(age)
+    var number = "0705032709"
+    println(number)
+    var weight = "52"
+    println (weight)
+
+    val iskenyan = true
+    val isnotkenyan = false
+
+    println (iskenyan) // output is true
+    println (isnotkenyan) // output is false
+}
