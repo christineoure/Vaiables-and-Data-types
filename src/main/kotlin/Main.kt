dc6c7034ba6117b364fc7d675bf79a04aa96ee2e
christineoure <99391324+christineoure@users.noreply.github.com>
2022-02-13 10:16:33 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,33 @@
 fun main(args: Array<String>) {
 
-    var name = "Santana Miranda"
-    println (name)
-    var age = "21"
-    println(age)
-    var number = "0705032709"
-    println(number)
-    var weight = "52"
-    println (weight)
 
-    val iskenyan = true
-    val isnotkenyan = false
+    var name = arrayOf("Santana Miranda", "Misanta Makeda", "Hope Hawi")
+    var age = arrayOf("16", "17", "18")
+    var number = arrayOf("0712022022", "0713022022", "0712345678")
+    var weight = arrayOf("56kg", "57kg", "58kg")
+    var kenyan = arrayOf("false", "true", "false")
 
-    println (iskenyan) // output is true
-    println (isnotkenyan) // output is false
+//    First student record
+    println(name[0])
+    println(age[0])
+    println(number[0])
+    println(weight[0])
+    println(kenyan[0])
+
+
+//    Second student record
+        println(name[1])
+        println(age[1])
+        println(number[1])
+        println(weight[1])
+        println(kenyan[1])
+
+
+//    Third student record
+        println(name[2])
+        println(age[2])
+        println(number[2])
+        println(weight[2])
+        println(kenyan[2])
 }
+
